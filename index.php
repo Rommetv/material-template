30c9c9661e287ac57d5ec7dacfd5b33a31057a51
Rommetv <Rommetv@users.noreply.github.com>
2016-04-01 19:08:00 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,304 @@
+<!--  Created by Romme te Velde 2016 -->
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Materialize CSS & style.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- META Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="The best parallax one page scrolling template. Based on Google's material design."> 
+    <meta name="keywords" content="material, parallax, one, page, pager, scrolling, custom, form, scroll">
+    <meta name="copyright" content="&copy; Copyright 2016 Romme te Velde" />
+    <title>Material Design</title>
+  </head>
+  <body>
+    <a name="top"></a>
+    <!-- Navigation -->
+<div class="navbar-fixed" id="nav">  
+  <nav class="z-depth-0">
+    <div class="nav-wrapper indigo z-depth-0">
+      <a href="#top" class="brand-logo">&ensp;Material</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#top">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="#top">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+<!-- Header -->
+      <header class="z-depth-2 indigo" id="header">
+        <!-- Change the words in js/typer.js for the typed words -->
+        <h3 class="white-text">Material design is <span class="typer"></span></h3>
+        <a href="#about" class="btn-floating btn-large waves-effect waves-light pink accent-2 scrollbutton z-depth-4"><i class="material-icons">keyboard_arrow_down</i></a>
+      </header>
+      <main>
+        <!-- Home -->
+  <div class="container">
+    <div class="section">
+      <h3 class="header center indigo-text">Material Design Template</h3>
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center indigo-text "><i class="large material-icons">palette</i></h2>
+            <h5 class="center">Material Design</h5>
+
+            <p class="light">Everyday carry four dollar toast raw denim single-origin coffee swag, microdosing plaid lumbersexual meh YOLO yr quinoa kogi. Slow-carb marfa pug, fingerstache tote bag tousled gastropub 90's yr biodiesel ramps. Umami +1 dreamcatcher, cronut tacos pork belly pug. Heirloom whatever marfa wolf, cardigan leggings deep v kinfolk skateboard yr semiotics. Ennui listicle ramps farm-to-table VHS chambray brooklyn cray, forage sartorial pop-up. Sriracha godard selvage, post-ironic four loko four dollar toast try-hard fanny pack heirloom portland yuccie. Banjo knausgaard marfa fixie distillery trust fund, umami schlitz.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center indigo-text"><i class="large material-icons">devices</i></h2>
+            <h5 class="center">Responsive</h5>
+
+            <p class="light">Kitsch whatever green juice williamsburg art party, seitan neutra pabst humblebrag before they sold out. Bushwick gastropub kale chips, brunch skateboard artisan semiotics blog migas. Cornhole chia roof party tacos, kinfolk listicle skateboard dreamcatcher godard butcher iPhone. Everyday carry polaroid bitters brunch marfa heirloom, chillwave forage keffiyeh man braid. Banh mi salvia mustache, dreamcatcher taxidermy cold-pressed fashion axe migas. Marfa blog cold-pressed locavore bicycle rights art party. Viral messenger bag ennui, 90's art party cold-pressed pork belly before they sold out man bun YOLO ugh tofu portland street art.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center indigo-text"><i class="large material-icons">weekend</i></h2>
+            <h5 class="center">Smooth Scrolling</h5>
+
+            <p class="light">Kitsch whatever green juice williamsburg art party, seitan neutra pabst humblebrag before they sold out. Bushwick gastropub kale chips, brunch skateboard artisan semiotics blog migas. Cornhole chia roof party tacos, kinfolk listicle skateboard dreamcatcher godard butcher iPhone. Everyday carry polaroid bitters brunch marfa heirloom, chillwave forage keffiyeh man braid. Banh mi salvia mustache, dreamcatcher taxidermy cold-pressed fashion axe migas. Marfa blog cold-pressed locavore bicycle rights art party. Viral messenger bag ennui, 90's art party cold-pressed pork belly before they sold out man bun YOLO ugh tofu portland street art.<p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  </div>
+  <!-- Parallax img 1 -->
+  <div class="parallax-container">
+    <a name="about"></a>
+    <div class="parallax"><img src="img/image1.jpg"></div>
+  </div>
+<!-- About -->
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">About</h2>
+      <blockquote>Ennui listicle ramps farm-to-table VHS chambray brooklyn cray, forage sartorial pop-up. Sriracha godard selvage, post-ironic four loko four dollar toast try-hard fanny pack heirloom portland yuccie. Banjo knausgaard marfa fixie distillery trust fund, umami schlitz.</blockquote>
+  <div class="row">
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-content">
+          <img src="img/face.jpg" class="responsive-img">
+          <h5 class="indigo-text">Romme te Velde</h5>
+          <h6 class="accent-text">CEO</h6>
+          <p>Everyday carry polaroid bitters brunch marfa heirloom, chillwave forage keffiyeh man braid. Banh mi salvia mustache, dreamcatcher taxidermy cold-pressed fashion axe migas. Marfa blog cold-pressed locavore bicycle rights art party. Viral messenger bag ennui, 90's art party cold-pressed pork belly before they sold out man bun YOLO ugh tofu portland street art.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://github.com/rommetv" target="_blank"><i class="fa fa-github accent-text"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-content">
+          <img src="img/face.jpg" class="responsive-img">
+          <h5 class="indigo-text">Romme te Velde</h5>
+          <h6 class="accent-text">CFO</h6>
+          <p>Everyday carry polaroid bitters brunch marfa heirloom, chillwave forage keffiyeh man braid. Banh mi salvia mustache, dreamcatcher taxidermy cold-pressed fashion axe migas. Marfa blog cold-pressed locavore bicycle rights art party. Viral messenger bag ennui, 90's art party cold-pressed pork belly before they sold out man bun YOLO ugh tofu portland street art.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://github.com/rommetv" target="_blank"><i class="fa fa-github accent-text"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m4 l4">
+      <div class="card">
+        <div class="card-content">
+          <img src="img/face.jpg" class="responsive-img">
+          <h5 class="indigo-text">Romme te Velde</h5>
+          <h6 class="accent-text">CTO</h6>
+          <p>Everyday carry polaroid bitters brunch marfa heirloom, chillwave forage keffiyeh man braid. Banh mi salvia mustache, dreamcatcher taxidermy cold-pressed fashion axe migas. Marfa blog cold-pressed locavore bicycle rights art party. Viral messenger bag ennui, 90's art party cold-pressed pork belly before they sold out man bun YOLO ugh tofu portland street art.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://github.com/rommetv" target="_blank"><i class="fa fa-github accent-text"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- Parallax img 2 -->
+  <div class="parallax-container">
+  <a name="work"></a>
+    <div class="parallax"><img src="img/image2.jpg"></div>
+  </div>
+  <!-- Work -->
+<div class="section white">
+  <div class="row container">
+    <h2 class="header">Work</h2>
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+  </div>
+  <div class="row container">
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+    <div class="col s12 m4 l4">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/portfolio.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator indigo-text">Picture<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Beautiful picture bullshit.</p>
+    </div>
+  </div>      
+    </div>
+  </div>
+</div>
+<!-- Contact & footer -->
+        <footer class="page-footer indigo">
+          <div class="container">
+            <div class="row">
+    <form class="col s12 m6 l6">
+      <h5 class="white-text"><a name="contact" class="white-text">Contact</a></h5>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="first_name" type="text" class="validate">
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <a class="waves-effect waves-light btn pink accent-2">Send</a>
+        </div>
+      </div>
+    </form>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright indigo darken-2">
+            <div class="container">
+            &copy; 2016 <a href="http://romme.tv">Romme te Velde</a>
+            <a class="grey-text text-lighten-4 right" href="https://github.com/rommetv" target="_blank"><i class="fa fa-github"></i></a>
+            </div>
+          </div>
+        </footer>
+      </main>
+    <!-- jQuery & materialize JS & SmoothScroll Plugins-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-localScroll/1.4.0/jquery.localScroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.js"></script>
+    <script src="js/typer.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,25 @@
+$( document ).ready(function(){
+      $('.parallax').parallax();
+      $('.button-collapse').sideNav();
+  var $window = $(window);
+  var scrollTime = 0.6;
+  var scrollDistance = 250;
+
+  $window.on("mousewheel DOMMouseScroll", function(event){
+
+    event.preventDefault(); 
+
+    var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+    var scrollTop = $window.scrollTop();
+    var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+    TweenMax.to($window, scrollTime, {
+      scrollTo : { y: finalScroll, autoKill:true },
+        ease: Power1.easeOut,
+        overwrite: 5              
+      });
+
+  });
+   $('#nav').localScroll({duration:800});
+   $('#header').localScroll({duration:800});
+})
